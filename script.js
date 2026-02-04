12bf843d2980193787530fe49f439e907b88e4d6
Yicheng <kou.isei@higaki.co.jp>
2026-02-04 21:09:19 +0900

    Add PC navigation menu

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const menuBtn = document.getElementById("menuBtn");
+const siteNav = document.getElementById("siteNav");
+
+menuBtn.addEventListener("click", () => {
+  const isOpen = siteNav.classList.toggle("is-open");
+  menuBtn.setAttribute("aria-expanded", String(isOpen));
+});
 
